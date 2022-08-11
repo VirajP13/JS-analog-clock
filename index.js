ec8f59d1d5606fa9b8209cd7ebfe828ca406fef0
VirajP13 <73154637+VirajP13@users.noreply.github.com>
2022-08-11 12:56:44 +0530

    add analog clock files

---
+++ index.js
@@ -0,0 +1,22 @@
+    
+setInterval(()=>{
+   
+//    let hour = document.getElementsByClassName("hour");
+//    let minute = document.getElementsByClassName("minute");
+//    let seconds = document.getElementsByClassName("seconds");
+
+
+    date = new Date();
+    htime = date.getHours();
+    mtime = date.getMinutes();
+    stime = date.getSeconds();
+
+    hrotation = 30*htime + mtime/2 ;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
